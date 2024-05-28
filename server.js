29bb871f42e4d7d15c4838c20631b49573d02623
Guilherme Dallacqua <53446795804@escola928.edu>
2024-05-28 17:20:51 -0300

    sla

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+db.connect(err => {
+    if(err){
+        console.error(
+            "Erro ao conectar com o banco de dados", err);
+            return;
+    } 
+    console.log('Conectado com o banco de dados 👍')
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
